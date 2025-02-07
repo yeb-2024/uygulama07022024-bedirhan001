b9318aec2b7ae43c6d4c49f1b6806b24d361abd6
bedirhan001 <151651543+bedirhan001@users.noreply.github.com>
2025-02-07 18:30:50 +0300

    add deadline

---
+++ main/src/anaSayfa.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class anaSayfa {
+
+    JFrame frame = new JFrame("Anasayfa");
+
+    public anaSayfa(){
+        frame.setSize(500,500);
+        frame.setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+
+        frame.setVisible(true);
+    }
+}
