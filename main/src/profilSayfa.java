e2826bb624b8dda7041ae97bc210929535c1cac6
bedirhan001 <151651543+bedirhan001@users.noreply.github.com>
2025-02-07 19:03:03 +0300

    resim png eklendi

--- main/src/profilSayfa.java
+++ main/src/profilSayfa.java
@@ -4,15 +4,24 @@ import java.awt.*;
 public class profilSayfa {
     JFrame frame = new JFrame();
     JLabel label = new JLabel();
+    JLabel imageLabel = new JLabel();
 
     public profilSayfa(){
         label.setText("Bedirhan Karaman");
         label.setFont(new Font("arial",Font.PLAIN,25));
+
+
+        ImageIcon icon = new ImageIcon("1.png");
+        Image img = icon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+        imageLabel.setIcon(new ImageIcon(img));
+        imageLabel.setHorizontalAlignment(JLabel.CENTER);
+
+        frame.add(imageLabel);
         frame.add(label);
 
         frame.setSize(500,500);
         frame.setTitle("Profil sayfası");
-        frame.setLayout(new GridLayout(3,1));
+        frame.setLayout(new GridLayout(2,1));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.setVisible(true);
