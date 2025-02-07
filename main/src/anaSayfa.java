de0556686b222d30d65638bf4e59686eadf8d238
bedirhan001 <151651543+bedirhan001@users.noreply.github.com>
2025-02-07 18:34:37 +0300

    add deadline

--- main/src/anaSayfa.java
+++ main/src/anaSayfa.java
@@ -1,16 +1,29 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class anaSayfa {
+public class anaSayfa implements ActionListener {
 
     JFrame frame = new JFrame("Anasayfa");
+    JButton button = new JButton("Profili Göster");
 
     public anaSayfa(){
+
+        button.addActionListener(this);
+        button.setBackground(Color.lightGray);
+        button.setBounds(200,200,100,50);
+        frame.add(button);
+
         frame.setSize(500,500);
         frame.setLayout(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-
-
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
